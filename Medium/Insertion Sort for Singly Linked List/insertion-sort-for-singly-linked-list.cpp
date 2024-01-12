1f7fdc14d43a85fe258c0081187670c0e3ec281b
ishaantiku01172 <153809197+ishaantiku01172@users.noreply.github.com>
2024-01-13 00:40:14 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Insertion Sort for Singly Linked List/insertion-sort-for-singly-linked-list.cpp
@@ -0,0 +1,115 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+/* Link list node */
+struct Node {
+	int data;
+	struct Node *next;
+	Node(int x) {
+		data = x;
+		next = NULL;
+	}
+};
+
+/* Function to print linked list */
+void printList(struct Node *head)
+{
+	struct Node *temp = head;
+	while (temp != NULL)
+	{
+		printf("%d ", temp->data);
+		temp = temp->next;
+	}
+}
+
+
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+/*Link list node
+struct Node {
+	int data;
+	struct Node *next;
+	Node(int x) {
+		data = x;
+		next = NULL;
+	}
+};*/
+
+class Solution {
+public:
+    Node* insertionSort(Node* head_ref) {
+        if (!head_ref || !head_ref->next) {
+            return head_ref; // Already sorted or empty list
+        }
+
+        Node* sorted = nullptr;
+        Node* current = head_ref;
+
+        while (current) {
+            Node* nextNode = current->next;
+
+            if (!sorted || sorted->data > current->data) {
+                // Insert at the beginning of the sorted list
+                current->next = sorted;
+                sorted = current;
+            } else {
+                // Traverse the sorted list to find the correct position
+                Node* temp = sorted;
+                while (temp->next && temp->next->data < current->data) {
+                    temp = temp->next;
+                }
+                current->next = temp->next;
+                temp->next = current;
+            }
+
+            current = nextNode;
+        }
+
+        return sorted;
+    }
+};
+//{ Driver Code Starts.
+int main()
+{
+	int T;
+	cin >> T;
+
+	while (T--)
+	{
+		int n;
+		cin >> n;
+
+		Node *head = NULL;
+		Node *temp = head;
+
+		for (int i = 0; i < n; i++) {
+			int data;
+			cin >> data;
+			if (head == NULL)
+				head = temp = new Node(data);
+			else
+			{
+				temp->next = new Node(data);
+				temp = temp->next;
+			}
+		}
+
+        Solution ob;
+
+		head = ob.insertionSort(head);
+		printList(head);
+
+		cout << "\n";
+
+
+
+	}
+	return 0;
+}
+// } Driver Code Ends
