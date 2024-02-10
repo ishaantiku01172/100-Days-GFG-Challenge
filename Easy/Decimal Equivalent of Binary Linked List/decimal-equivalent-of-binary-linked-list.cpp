4cb1311dbc4ae7916cd4ada4bc87fd0682c09e6e
ishaantiku01172 <153809197+ishaantiku01172@users.noreply.github.com>
2024-02-10 14:26:36 +0530

    Updated Solution - GfG to GitHub

--- Easy/Decimal Equivalent of Binary Linked List/decimal-equivalent-of-binary-linked-list.cpp
+++ Easy/Decimal Equivalent of Binary Linked List/decimal-equivalent-of-binary-linked-list.cpp
@@ -19,29 +19,11 @@ struct Node
 
 
 // } Driver Code Ends
-/* Below global variable is declared in code for modulo arithmetic
-const long long unsigned int MOD = 1000000007; */
-
-/* Link list Node/
-struct Node
-{
-    bool data;   // NOTE data is bool
-    Node* next;
-    
-    Node(int x){
-        data = x;
-        next = NULL;
-    }
-    
-}; */
-
 class Solution
 {
     public:
-        // Should return decimal equivalent modulo 1000000007 of binary linked list 
-        long long unsigned int decimalValue(Node *head)
+         long long unsigned int decimalValue(Node *head)
         {
-           // Your Code Here
            long long unsigned int val=0;
            Node*curr=head;
            while(curr)
