8fd209fe65baa94807abd8c2fe4823f0a33a0df9
ishaantiku01172 <153809197+ishaantiku01172@users.noreply.github.com>
2024-02-11 01:39:56 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Recamans sequence/recamans-sequence.cpp
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+class Solution{
+public:
+
+   
+    vector<int> recamanSequence(int n){
+        // code here
+        vector<int>ans(n,-1);
+        unordered_set<int>us;
+        ans[0] = 0;
+        for(int i=1;i<n;i++){
+            int prev = ans[i-1];
+            if(prev - i > 0 && us.find(prev - i) == us.end()){
+            ans[i] = prev -i;
+            }
+            else ans[i] = prev+i;
+            
+            us.insert(ans[i]);
+        }
+        return ans;
+        
+    }
+};
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        
+        Solution ob;
+        vector<int> ans = ob.recamanSequence(n);
+        for(int i = 0;i < n;i++)
+            cout<<ans[i]<<" ";
+        cout<<"\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
